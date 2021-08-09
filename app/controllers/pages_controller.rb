f9264aa3d1e856f5c98531b819baca8cdb494377
Marcel Fonseca <marcelsf23br@gmail.com>
2021-08-09 10:38:13 +0200

    finsihed for batch 668

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,16 @@
 class PagesController < ApplicationController
+  def about
+    @staff = ['Dion', 'Wiebke','Marcel']
+    @students = ['Max', 'Friedrich', 'Louis L', 'Louis N', 'Oscar', 'Janus' ]
+
+    if params[:member]
+      @students = @students.select {|student| student == params[:member]}
+    end
+  end
+
+  def contact
+  end
+
+  def home
+  end
 end
